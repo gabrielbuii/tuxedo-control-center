b8b5b0c5a003485f79eb067dddbbb4cc09a89844
Gabriel Bui <bui.gabor97@gmail.com>
2024-09-20 13:11:35 +0200

    test

--- src/service-app/main.ts
+++ src/service-app/main.ts
@@ -20,9 +20,10 @@
 /**
  * Start point of TUXEDO Control Center Service
  */
-import { TuxedoControlCenterDaemon } from './classes/TuxedoControlCenterDaemon';
+import { TuxedoControlCenterDaemon } from "./classes/TuxedoControlCenterDaemon";
 
 const tccd = new TuxedoControlCenterDaemon();
 
+console.log("test");
 // Start program
 tccd.main().catch((err) => tccd.catchError(err));
